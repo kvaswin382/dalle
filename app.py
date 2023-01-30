04046c496a1395dda3deb4a1acf69ab55448cade
kvaswin382 <87608397+kvaswin382@users.noreply.github.com>
2023-01-30 18:42:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,56 @@
+import requests
+import urllib.parse as ulib
+from flask import Flask,jsonify,request
+from requests.structures import CaseInsensitiveDict
+
+
+app = Flask(__name__)
+
+def generate_image(prompt, model, api_key):
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+    api_key = "Bearer " + api_key
+
+    headers["Authorization"] = api_key
+
+    model = model
+    prompt = prompt
+
+    model_engine = "image-alpha-001"
+    data = """
+    {
+        """
+    data += f'"model": "{model_engine}",'
+    data += f'"prompt": "{prompt}",'
+    data += """
+        "num_images":1,
+        "size":"1024x1024",
+        "response_format":"url"
+    }
+    """
+
+    resp = requests.post(QUERY_URL, headers=headers, data=data)
+
+    if resp.status_code != 200:
+        return False
+
+    response_text = json.loads(resp.text)
+    return response_text['data'][0]['url']
+
+
+@app.route('/')
+def dalle():
+    prompt = ulib.unquote(request.args.get('prompt',1))
+    api_key = request.args.get('api_key',1)
+    img_url = generate_image(prompt,'image-alpha-001',api_key)
+    if not img_url:
+        data = {
+          'ok': False,
+          'message': 'Cant generate image'
+        }
+        return jsonify()
+    data = {
+      'ok': True,
+      'image_url': img_url
+    }
+    return jsonify(data)
